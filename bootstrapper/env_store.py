6e7f6060c8009cc8ccaec0c31a4937818a006d4a
Romain Mormont <romain.mormont@cytomine.com>
2023-01-05 10:25:46 +0100

    fix: global variables mgmt

--- bootstrapper/env_store.py
+++ bootstrapper/env_store.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import enum
 from collections import defaultdict
 
@@ -10,31 +11,45 @@ class EnvValueTypeEnum(enum.Enum):
   AUTOGENERATE = "auto"
 
 
-class EnvStore:
+class KeyAlreadyExistsError(KeyError):
+  def __init__(self, ns, key, *args: object) -> None:
+    super().__init__(f"key '{key}' already exists in namespace '{ns}'", *args)
+
+
+class BaseEnvStore(ABC):
+  @abstractmethod
+  def get_value(namespace: str, key: str):
+    """Get a value stored in the EnvStore"""
+    pass
+
+
+class EnvStore(BaseEnvStore):
   def __init__(self) -> None:
     self._store = defaultdict(dict)
     self._store_type = defaultdict(dict)
   
   def _add_env(self, ns, key, value_fn, _type):
+    if key in self._store[ns]:
+      raise KeyAlreadyExistsError(ns, key)
     self._store[ns][key] = value_fn()
     self._store_type[ns][key] = _type
 
-  def add_namespace(self, ns: str, d: dict, store=None):
+  def add_namespace(self, ns: str, entries: dict, store=None):
     if ns in self._store:
       raise ValueError(f"namespace '{ns}' already exists in env store")
     
-    for k, v in d.get(EnvValueTypeEnum.CONSTANT.value, {}).items():
+    for k, v in entries.get(EnvValueTypeEnum.CONSTANT.value, {}).items():
       self._add_env(ns, k, lambda: v, EnvValueTypeEnum.CONSTANT)
     
-    if EnvValueTypeEnum.GLOBAL.value in d and len(d[EnvValueTypeEnum.GLOBAL.value]) > 0 and store is None:
+    if EnvValueTypeEnum.GLOBAL.value in entries and len(entries[EnvValueTypeEnum.GLOBAL.value]) > 0 and store is None:
       raise ValueError("'global' is not supported in this section")
 
-    for k, v in d.get(EnvValueTypeEnum.GLOBAL.value, {}):
+    for k, v in entries.get(EnvValueTypeEnum.GLOBAL.value, {}).items():
       other_ns, other_key = v.split(".")
-      self._add_env(ns, k, lambda: store.get(other_ns, other_key), EnvValueTypeEnum.GLOBAL)
+      self._add_env(ns, k, lambda: store.get_value(other_ns, other_key), EnvValueTypeEnum.GLOBAL)
 
     gen_factory = EnvValueGeneratorFactory()
-    for k, v in d.get(EnvValueTypeEnum.AUTOGENERATE.value, {}):
+    for k, v in entries.get(EnvValueTypeEnum.AUTOGENERATE.value, {}).items():
       generator = gen_factory.make_generator(v)
       self._add_env(ns, k, lambda: generator.resolve(v), EnvValueTypeEnum.AUTOGENERATE)
       
