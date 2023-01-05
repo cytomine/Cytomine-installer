97f19ac8e93d7c09adb42942fd7ffe6f570d7d6b
Romain Mormont <romain.mormont@cytomine.com>
2023-01-05 18:24:01 +0100

    feat: implement as dict method in env_store

--- bootstrapper/env_store.py
+++ bootstrapper/env_store.py
@@ -22,40 +22,62 @@ class BaseEnvStore(ABC):
     """Get a value stored in the EnvStore"""
     pass
 
+  @abstractmethod
+  def as_dict(self):
+    """Generates a directory"""
+    pass
 
 class EnvStore(BaseEnvStore):
   def __init__(self) -> None:
     self._store = defaultdict(dict)
-    self._store_type = defaultdict(dict)
+    self._initial_type = defaultdict(dict)
+    self._initial_value = defaultdict(dict)
   
-  def _add_env(self, ns, key, value_fn, _type):
+  def _add_env(self, ns: str, key: str, value, store_value_fn, _type: EnvValueTypeEnum):
+    """Register an envirom"""
     if key in self._store[ns]:
       raise KeyAlreadyExistsError(ns, key)
-    self._store[ns][key] = value_fn()
-    self._store_type[ns][key] = _type
+    self._store[ns][key] = store_value_fn()
+    self._initial_type[ns][key] = _type
+    self._initial_value[ns][key] = value
 
   def add_namespace(self, ns: str, entries: dict, store=None):
     if ns in self._store:
       raise ValueError(f"namespace '{ns}' already exists in env store")
     
     for k, v in entries.get(EnvValueTypeEnum.CONSTANT.value, {}).items():
-      self._add_env(ns, k, lambda: v, EnvValueTypeEnum.CONSTANT)
+      self._add_env(ns, k, v, lambda: v, EnvValueTypeEnum.CONSTANT)
     
     if EnvValueTypeEnum.GLOBAL.value in entries and len(entries[EnvValueTypeEnum.GLOBAL.value]) > 0 and store is None:
       raise ValueError("'global' is not supported in this section")
 
     for k, v in entries.get(EnvValueTypeEnum.GLOBAL.value, {}).items():
       other_ns, other_key = v.split(".")
-      self._add_env(ns, k, lambda: store.get_value(other_ns, other_key), EnvValueTypeEnum.GLOBAL)
+      self._add_env(ns, k, v, lambda: store.get_value(other_ns, other_key), EnvValueTypeEnum.GLOBAL)
 
     gen_factory = EnvValueGeneratorFactory()
     for k, v in entries.get(EnvValueTypeEnum.AUTOGENERATE.value, {}).items():
       generator = gen_factory.make_generator(v)
-      self._add_env(ns, k, lambda: generator.resolve(v), EnvValueTypeEnum.AUTOGENERATE)
+      self._add_env(ns, k, v, lambda: generator.resolve(v), EnvValueTypeEnum.AUTOGENERATE)
       
+  @property
+  def namespaces(self):
+    return list(self._store.keys())
+
   def get_value(self, ns: str, key: str):
     if ns not in self._store:
       raise ValueError(f"unknown namespace '{ns}'")
     if key not in self._store[ns]:
       raise ValueError(f"unknown key '{key}' in namespace '{ns}'")
     return self._store[ns][key]
+
+  def as_dict(self):
+    output_dict = defaultdict(lambda: defaultdict(dict))
+    for ns in self.namespaces:
+      for key in self._store[ns].keys():
+        init_type, init_value = self._initial_type[ns][key], self._initial_value[ns][key]
+        if init_type == EnvValueTypeEnum.AUTOGENERATE and (isinstance(init_value, str) or init_value["freeze"]):
+          output_dict[ns][EnvValueTypeEnum.CONSTANT.value][key] = self._store[ns][key]
+        else:
+          output_dict[ns][init_type.value][key] = self._initial_value[ns][key]
+    return output_dict
