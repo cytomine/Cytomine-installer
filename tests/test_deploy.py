a836dc4201eb5a3c0dc807af296bdc708704a79d
Romain Mormont <romain.mormont@cytomine.com>
2023-05-11 18:23:06 +0200

    test: test template only and auto field

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -7,6 +7,8 @@ from tempfile import TemporaryDirectory
 import unittest
 import zipfile
 
+import yaml
+
 from cytomine_installer import parser
 from cytomine_installer.actions.errors import InvalidTargetDirectoryError
 from cytomine_installer.deployment.deployment_folders import InvalidServerConfigurationError
@@ -123,7 +125,35 @@ class TestDeploy(TestDeploymentGeneric):
         with TemporaryDirectory() as tmpdir:
             parser.call(["deploy", "-s", deploy_file_path, "-t", tmpdir])
             self.assertSameDirectories(tmpdir, output_ref_path)
+    
+    def testDeploySingleServerTemplateOnlyAndAuto(self):
+        tests_path = os.path.dirname(__file__)
+        deploy_file_path = os.path.join(tests_path, "files", "fake_single_server_template_only_with_auto", "in")
+        output_ref_path = os.path.join(tests_path, "files", "fake_single_server_template_only_with_auto", "out")
+        with TemporaryDirectory() as tmpdir:
+            parser.call(["deploy", "-s", deploy_file_path, "-t", tmpdir])
             
+            # read generated value from .env
+            with open(os.path.join(tmpdir, ".env"), "r", encoding="utf8") as file:
+                var_name, var_value = file.read().strip().split("=")
+                self.assertEqual(var_name, "NS1_VAR1")
+                self.assertRegex(var_value, UUID_PATTERN)
+            
+            with open(os.path.join(tmpdir, "cytomine.yml"), "r", encoding="utf8") as file:
+                yml_content = yaml.load(file, Loader=yaml.Loader)
+                self.assertRegex(yml_content["global"]["ns1"]["constant"]["VAR1"], UUID_PATTERN)
+                self.assertEqual(yml_content["global"]["ns1"]["constant"]["VAR1"], var_value) 
+
+            self.assertSameYamlFileContent(
+                os.path.join(tmpdir, "cytomine.template"),
+                os.path.join(output_ref_path, "cytomine.template")
+            )
+            
+            with open(os.path.join(tmpdir, "cytomine.template"), "r", encoding="utf8") as file:
+                yml_content = yaml.load(file, Loader=yaml.Loader)
+                self.assertRegex(yml_content["global"]["ns1"]["auto"]["VAR1"], "random_uuid")
+
+
     @unittest.skip("implement later")
     def testMultiServer(self):
         tests_path = os.path.dirname(__file__)
